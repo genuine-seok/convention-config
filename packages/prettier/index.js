f8d3df0596deddaab5fff6c83fe8efc50eb10d12
genuine-seok <genuineseok@gmail.com>
2023-09-06 18:42:29 +0900

    feat: set prettier config package
    - publish @jinseokkim/prettier-config first release version (1.0.0)

---
+++ packages/prettier/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  $schema: "http://json.schemastore.org/prettierrc",
+  printWidth: 80,
+  tabWidth: 2,
+  arrowParens: "always",
+  trailingComma: "all",
+  endOfLine: "lf",
+  useTabs: false,
+  singleQuote: true,
+  semi: true,
+  insertPragma: false,
+  bracketSpacing: true,
+};
