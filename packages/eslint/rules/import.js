47007bfc8d89d40be191c0fe1cd50eb7ca3b16c5
genuine-seok <genuineseok@gmail.com>
2023-09-07 19:02:08 +0900

    feat: update new eslint rule(import/no-absolute-path)

--- packages/eslint/rules/import.js
+++ packages/eslint/rules/import.js
@@ -76,5 +76,6 @@ module.exports = {
      * @link https://github.com/import-js/eslint-plugin-import/blob/v2.27.5/docs/rules/no-mutable-exports.md
      */
     "import/no-mutable-exports": "off",
+    "import/no-absolute-path": "off",
   },
 };
